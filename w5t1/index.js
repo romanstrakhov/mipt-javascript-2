f47fba05f5fce3412a19d5f4f6323d2b6148b55b
Roman Strakhov <roman@strakhov.ru>
2018-02-09 22:01:10 +0300

    course finished

--- w5t1/index.js
+++ w5t1/index.js
@@ -1,11 +1,25 @@
 module.exports = {
 
+    subscription: [],
+
     /**
      * @param {String} event
      * @param {Object} subscriber
      * @param {Function} handler
      */
     on: function (event, subscriber, handler) {
+
+            // console.log('*** on:');
+
+            this.subscription.push({
+                event: event,
+                subscriber: subscriber,
+                handler: handler
+            });
+
+            // console.log(this.subscription);
+
+            return this;
 
     },
 
@@ -15,12 +29,47 @@ module.exports = {
      */
     off: function (event, subscriber) {
 
+            // console.log('*** off:');
+
+            this.subscription = this.subscription.filter(function(e, i, a) {
+                if ( this.event === e.event & this.subscriber === e.subscriber ) {
+                    return false;
+                } else {
+                    return true;
+                }
+            },{
+                event: event,
+                subscriber: subscriber
+            });
+
+            // subscription.push({event: event, subscriber: subscriber});
+
+            // console.log(this.subscription);
+
+            return this;
+
+
+
+
     },
 
     /**
      * @param {String} event
      */
     emit: function (event) {
+
+        // console.log('*** emmit:');
+
+        this.subscription.forEach( function (v) {
+            // console.log('\t?:', v.event, ' vs. ', this.event);
+            if ( v.event === this.event ) {
+                v.handler.call(v.subscriber);
+            // console.log('\t>:', v.subscriber, ': ', v.handler);
+
+            }
+        }, {event: event} );
+
+        return this;
 
     }
 };
